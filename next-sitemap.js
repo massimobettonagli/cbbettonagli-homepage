4bdae1d24348491719cd70120a5a028aac645144
Massimo Bettonagli <massimobettonagli@iMac-di-Massimo.local>
2025-10-13 08:25:16 +0200

    Fix next-sitemap config and exclude privacy-policy

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,11 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const excludePaths = (process.env.NEXT_PUBLIC_SITEMAP_EXCLUDE || '/privacy-policy').split(',');
+
 module.exports = {
   siteUrl: 'https://www.cbbettonagli.it',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/privacy-policy'], // se vuoi escludere qualche pagina
+  exclude: excludePaths, // può essere dinamica o fissa
   robotsTxtOptions: {
     policies: [
       {
